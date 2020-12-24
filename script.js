0aca41701207b018b833289bfc1931f7e9068fa6
Dan Bushong <bushongdan@gmail.com>
2020-12-24 13:24:46 -0600

    button now prompts

--- script.js
+++ script.js
@@ -1,14 +1,44 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+var lowerCase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+
+var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+var numberString = ["0","1","2","3","4","5","6","7","8","9"];
+
+var specialCharacter = ["!","@","#","$","%","^","&","*","(",")","_","=","+"];
+
+
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
 
+function promptMe(){
+
+  var userCharacterCountChoice = prompt("How many characters?")
+
+  console.log(userCharacterCountChoice)
+  
+  
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+  
+  
+
+
+  
+
+
+
+
+//   function writePassword() {
+//     var password = generatePassword();
+//     var passwordText = document.querySelector("#password");
+
+//     passwordText.value = password;
+
+//   }
+
+// // Add event listener to generate button
+// generateBtn.addEventListener("click", writePassword);
